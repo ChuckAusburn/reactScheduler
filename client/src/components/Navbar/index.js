90fa1f1464362771a517d1ce0bf67a00b43ff764
Ahdimaccio <adimacci95@gmail.com>
2019-02-14 21:33:08 -0800

    calender updates 2.0

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -7,42 +7,48 @@ import "./style.css";
 function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <Link className="navbar-brand" to="/">
-        <FontAwesomeIcon
-          icon="gamepad"
-        />
-      </Link>
-      <div>
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <Link
-              to="/"
-              className={
-                window.location.pathname === "/" || window.location.pathname === "/about"
-                  ? "nav-link active"
-                  : "nav-link"
-              }
-            >
-              Home
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              to="/saved"
-              className={window.location.pathname === "/saved" ? "nav-link active" : "nav-link"}
-            >
-              Saved Events
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              to="/search"
-              className={window.location.pathname === "/search" ? "nav-link active" : "nav-link"}
-            >
-              Search
-            </Link>
-          </li>
-        </ul>
+        <Link className="navbar-brand" to="/">
+          <FontAwesomeIcon
+            icon="gamepad"
+          />
+        </Link>
+        <div>
+        <button className="navbar-toggler toggler-example" type="button" data-toggle="collapse" data-target="#navbarSupportedContent41" aria-controls="navbarSupportedContent41" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="white-text"><i className="fa fa-bars fa-1x"></i></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent41">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <Link
+                to="/"
+                className={
+                  window.location.pathname === "/" || window.location.pathname === "/about"
+                    ? "nav-link active"
+                    : "nav-link"
+                }
+              >
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/saved"
+                className={window.location.pathname === "/saved" ? "nav-link active" : "nav-link"}
+              >
+                Saved Events
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/search"
+                className={window.location.pathname === "/search" ? "nav-link active" : "nav-link"}
+              >
+                Search
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
