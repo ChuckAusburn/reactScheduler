3f80fa31e88621623cf0c211605782e552dc6297
Ahdimaccio <adimacci95@gmail.com>
2019-02-15 04:17:32 -0800

    more calendar updates

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -4,7 +4,11 @@ import "./style.css";
 function Footer() {
   return (
     <footer className="footer">
-      <span>Nerd Universe @ Copyright 2019</span>
+      <span>
+        Nerd Universe @ Copyright 2019
+        <br></br>
+        All Artwork Rights Reserved @ <a href="https://www.deviantart.com/strongstuff">StrongStuff</a> 2019
+      </span>
     </footer>
   );
 }
