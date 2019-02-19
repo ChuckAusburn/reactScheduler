81452f96039fb1011fc2af704136e370391f77e7
Ahdimaccio <adimacci95@gmail.com>
2019-02-18 20:32:06 -0800

    visual basics for add-events page

--- client/src/pages/Add.js
+++ client/src/pages/Add.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import "./index.css";
+import "./add.css";
 import Saved from "../components/Saved";
 import Row from "../components/Row";
 import Container from "../components/Container";
@@ -8,7 +8,6 @@ import {List,ListItem} from "../components/List";
 import DeleteBtn from "../components/DeleteBtn";
 import API from "../utils/API"
 //import {MDBContainer,MDBRow,MDBCol,MDBBtn,MDBIcon,MDBInput} from 'mdbreact';
-
 
 class Add extends Component {
    state = {
@@ -19,7 +18,6 @@ class Add extends Component {
      saved: []
    };
  
-
  handleInputChange = event => {
    const {
      name,
@@ -30,7 +28,6 @@ class Add extends Component {
    });
  };
    
-
   handleFormSubmit = event => {
     event.preventDefault();
     if (this.state.title && this.state.author) {
@@ -77,61 +74,14 @@ class Add extends Component {
 //     </MDBContainer>
 //   );
 // };
-
 // export default FormPage;
   
  render() {
     return(
-    <div>
-      <Saved backgroundImage="https://media.giphy.com/media/aqCQhTE5dCHo4/200.gif">
-        <h1>Add Events</h1>
-        <h2>Welcome to Los Angeles</h2>
-      </Saved>
-      <Container style={{ marginTop: 30 }}>
-        <Row>
-          <h4></h4>
-            <ul class="ch-grid">
-              <li>
-                <div className="ch-item ch-img-1">
-                  <div className="ch-info">
-                    <h3>Search</h3>
-                    <p>Hundreds of exciting events near you</p>
-                  </div>
-                </div>
-              </li>
-              <li>
-                <div class="ch-item ch-img-2">
-                  <div class="ch-info">
-                    <h3>Save</h3>
-                    <p>Your favorites to visit in the future</p>
-                  </div>
-                </div>
-              </li>
-              <li>
-                <div class="ch-item ch-img-3">
-                  <div class="ch-info">
-                    <h3>Send</h3>
-                    <p>Us your thoughts on how we can improve</p>
-                  </div>
-                </div>
-              </li>
-              <li>
-                <div class="ch-item ch-img-4">
-                  <div class="ch-info">
-                    <h3>Survive</h3>
-                    <p>Because being a nerd is a full-time job</p>
-                  </div>
-                </div>
-              </li>
-              </ul>
-        </Row>
-        <Row style={{ marginTop: 30 }}>
-          <h4>Upcoming Events</h4>
-          <Calendar/>
-        <br/>
-      
-        </Row>
-      </Container>
+    <div id="wrapper2">
+      <div className="header">
+        <h1>Add Event</h1>
+      </div>
     </div>
   );
 }
